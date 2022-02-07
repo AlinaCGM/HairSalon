a495079d04ab917c8e49ce0f3faf558b03e0f189
Alina <alinbogz@gmail.com>
2022-02-07 21:44:40 +0100

    google login done

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { auth, provider } from '../../firebase-config'
 import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import './Edits/Edit.css'
+import googleImage from "../../images/btn_google_signin_dark_normal_web@2x.png"
 // import { ContentSection } from '../Content'
 
 function Login({ setIsAuth }) {
@@ -16,10 +18,10 @@ function Login({ setIsAuth }) {
   }
 
   return (
-    <div className='loginPage'>
-      <p>Sign In With Google to Continue</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}>
-        Sign in with Google
+    <div className='loginPage mx-auto text-center w-100s '>
+      <div><h1 className='first-textLogin'>Welcome to admin page</h1></div>  
+      <button className='login-with-google-btn'  onClick={signInWithGoogle} >
+     <img src={googleImage} className='login-img' alt="" />
       </button>
     </div>
   )
